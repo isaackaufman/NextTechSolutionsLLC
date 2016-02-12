f6b31aa4085faf7267f09123de638a62d4e80808
Isaac Kaufman <ikaufman588@gmail.com>
2016-02-11 22:45:49 -0500

    Initial Commit (hopefully)

---
+++ app/src/main/java/solutions/nexttech/isaackaufman/nexttechsolutionsllc/MainActivity.java
@@ -0,0 +1,13 @@
+package solutions.nexttech.isaackaufman.nexttechsolutionsllc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

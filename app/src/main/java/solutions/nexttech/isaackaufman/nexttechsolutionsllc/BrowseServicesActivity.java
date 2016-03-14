a6629ef888fa48425257fe35f2f2b5e19569ce0c
Isaac Kaufman <ikaufman588@gmail.com>
2016-03-14 18:39:34 -0400

    Dynamically Load services into TextViews - WIP

--- app/src/main/java/solutions/nexttech/isaackaufman/nexttechsolutionsllc/BrowseServicesActivity.java
+++ app/src/main/java/solutions/nexttech/isaackaufman/nexttechsolutionsllc/BrowseServicesActivity.java
@@ -1,8 +1,12 @@
 package solutions.nexttech.isaackaufman.nexttechsolutionsllc;
 
 import android.app.Activity;
+import android.app.Service;
 import android.os.Bundle;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sweg on 3/13/2016.
@@ -17,18 +21,42 @@ public class BrowseServicesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_services);
 
-        final TextView textView = (TextView) findViewById(R.id.htmlText);
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+        ArrayList<TextView> tvList = new ArrayList<TextView>();
+
+
         GetServices servicesTask = (GetServices) new GetServices(new GetServices.AsyncResponse(){
             @Override
             public void processFinish (String output)
             {
                 // TODO process html into readable and formatted entries
-                textView.setText(output);
+                //for ()
+                //scrollView.addView();
             }
 
         }).execute("http://nexttech.solutions/home.html");
     }
 
+    private ArrayList<String> parseServices (String html)
+    {
+        ArrayList<String> services = new ArrayList<String>();
+        String service = "";
+        int startIndex;
+        int endIndex;
+
+        // keep track of where we are in the html document
+        int curPos = 0;
+        while ((startIndex = html.indexOf("<h2>", curPos)) != -1)
+        {
+            endIndex = html.indexOf("</h2", startIndex);
+            curPos = endIndex;
+
+            // trim off <h2> and </h2>
+            service = html.substring(startIndex + 4, endIndex);
+        }
+
+        return services;
+    }
 
 
 
